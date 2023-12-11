f4d811587f2786298f18956da01c77740cda0885
ᴷʳᵉˢˢʷᵉˡˡ <147948046+EscaliBud@users.noreply.github.com>
2023-12-11 13:02:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -163,6 +163,15 @@ async function startHisoka() {
          client.readMessages([mek.key]);
 
 }
+      if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
+      if (mek.key.id.startsWith("BAE5") && mek.key.id.length === 16) return;
+      m = smsg(client, mek, store);
+      const infinity = require("./infinity");
+infinity(client, m, chatUpdate, store);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 
  
