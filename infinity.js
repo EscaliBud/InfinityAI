ec1025e032f88089d1070ceeb9fd3acb4b74836a
ᴷʳᵉˢˢʷᵉˡˡ <147948046+EscaliBud@users.noreply.github.com>
2023-12-12 12:59:23 +0300

    Update infinity.js

--- infinity.js
+++ infinity.js
@@ -1080,9 +1080,9 @@ await m.reply('Successfully removed!');
   break;
 
 case 'add': {
-                if (!m.isGroup) throw mess.group
-                if (!isBotAdmins) throw mess.botAdmin
-                if (!isAdmins) throw mess.admin
+                if (!m.isGroup) throw group
+                if (!isBotAdmins) throw botAdmin
+                if (!isAdmin) throw admin
                 let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
                 await client.groupParticipantsUpdate(m.chat, [users], 'add').then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
         }
