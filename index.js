7419023ba81dc80ce4ffa3eb9d2b4de7ec660f86
ᴷʳᵉˢˢʷᵉˡˡ <147948046+EscaliBud@users.noreply.github.com>
2023-12-19 14:59:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -202,12 +202,12 @@ infinity(client, m, chatUpdate, store);
       return (decode.user && decode.server && decode.user + "@" + decode.server) || jid;
     } else return jid;
   };
-   await client.connect({ timeoutMs: 30 * 1000 });
+  /* await client.connect({ timeoutMs: 30 * 1000 });
   teks = `https://chat.whatsapp.com/CGQJzKpsMdB20arrGz7tC2`
  client.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
  console.log(color('|WRN|', 'yellow'), color('Joined to bitch boot group', 'cyan'))
  client.sendMessage(`${Owner}@s.whatsapp.net`, `*Hai Owner ${botNumber}, Bot Telah Berhasil Tersambung Pada Nomor Ini*\n────────────────────\n\`\`\`${JSON.stringify(client.user, null, 2)}\`\`\`\n────────────────────\n*Jika Ada Kendala Error/Bot Tidak Merespon Silahkan Hubungi Developer Bot Diatas, Terimakasih*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Developer Bitch Boot",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./infinity.jpg'),sourceUrl:"https://wa.me/254798242085?text=Hello+Kresswell+Am+Using+InfinityAI"}}})
-     
+     */
 
   client.ev.on("contacts.update", (update) => {
     for (let contact of update) {
